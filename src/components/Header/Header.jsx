aacb98c6f6680c0f85e026b09ac8c7289970bb33
Tzeela Belmaker <mador.till.mop@gmail.com>
2026-03-12 14:48:23 +0200

    home 3

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
-import "./Header.css"; 
+import React from 'react';
+import styles from './Header.module.css';
 import logo from "../../assets/img/logo.svg";
 import tilLogo from "../../assets/img/til-logo.svg";
 
 const Header = () => {
     return (
-        <div id="header-bg">
-            <img src={tilLogo} alt="mador Til logo" className="mador-til-logo"/>
-            <p>מלחמת אסטרואידים</p>
-            <img src={logo} alt="logo" className="logo"/>
-        </div>
+        <header className={styles.headerBg}>
+            <img src={tilLogo} alt="mador Til logo" className={styles.madorTilLogo}/>
+            <p className={styles.title}>מלחמת אסטרואידים</p>
+            <img src={logo} alt="logo" className={styles.logo}/>
+        </header>
     );
 };
 
